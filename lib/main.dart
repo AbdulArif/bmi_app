3acec46754369b11da4c52a2aaffee74330cdb89
AbdulArif <abdularif.dgp@gmail.com>
2023-10-08 10:53:55 +0530

    App bar clor change

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -72,6 +73,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         // backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: bgColor,
         title: Text("BMI Calculator"),
       ),
       body: Container(
